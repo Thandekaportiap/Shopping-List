cfe435972021bb243bcd4ad53e0de77714401ebc
Thandeka Portia P Mazibuko <thandekaportiap05@gmail.com>
2024-09-26 20:24:17 +0200

    new branch

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,25 +8,25 @@ const Hero = () => {
   return (
     <section
     style={{ 
-        backgroundImage: `url(${Logo})`, 
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center'
-      }} 
-      className='w-full h-screen'
-    >
-      
-   <div className='ml-5 flex flex-col justify-center items-center'>
-    
-    <h1 className='leading-loose text-6xl font-bold'>The best way<br/> to simplify<br/> your shopping </h1>
-    <h4>Here you can put your short description about your project</h4>
-    <div style={{marginTop:"2%", display:"flex", flexDirection:"row"}}>
+      backgroundImage: `url(${Logo})`, 
+      backgroundSize: 'cover', 
+      backgroundPosition: 'center' 
+    }} 
+    className='w-full h-screen flex items-center justify-between p-10'
+  >
+    <div className='w-1/2'>
+      <h1 className='text-8xl font-bold leading-tight'>
+        The best way<br /> to simplify<br /> your shopping
+      </h1>
+      <h4 className='mt-4 text-lg'>
+        Here you can put your short description about your project
+      </h4>
     </div>
-   </div>
-   <div>
-        <img src={HeroImg} alt="cubes" />
-   </div>
-
-    </section>
+    
+      <img src={HeroImg} alt="cubes" className='h-2/3 w-2/4 rounded-lg' />
+   
+  </section>
+  
   )
 }
 
