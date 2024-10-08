ddb6c91f4320c6c067cf2bdcc453a3bda15159eb
Thandekaportiap <thandekaportiap05@gmail.com>
2024-10-08 08:58:28 +0200

    chore: added shadow on displaylist card

--- client/src/pages/DisplayList.jsx
+++ client/src/pages/DisplayList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { fetchShoppingLists } from '../features/Shopping/ShoppingListSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import List from '../assets/list (1).png'
 
 const DisplayList = ({id}) => {
 
@@ -25,14 +26,16 @@ const DisplayList = ({id}) => {
   return (
     <>
    <section className='min-h-screen flex flex-col items-center justify-center'>
-   <p className="text-4xl md:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-pink-500 my-4">Shopping Lists</p>
+   <p className="text-4xl md:text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#B1C98D] to-[#C087BF] p-2 my-6">Shopping Lists</p>
    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
         {shoppingLists.map((list) => (
             <div 
                 key={list.id} 
-                className="transform rounded-xl h-40 w-full sm:h-64 shadow-xl transition duration-300 hover:scale-105 flex flex-col justify-between"
+                className="transform rounded-xl h-40 w-full sm:h-64 shadow-xl shadow-[#B1C98D] transition duration-300 hover:scale-105 flex flex-col justify-between"
             >
+                
                 <div className="flex-grow flex items-center justify-center text-center p-4">
+                <img src={List} alt="list" className="w-16 h-16 object-cover rounded-full" />
                     <h1 className='text-2xl font-bold'>{list.listName} List</h1>
                 </div>
                 <button 
